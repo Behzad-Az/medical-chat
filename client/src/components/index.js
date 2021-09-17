0efad282d0f1a7709f6e6c3696fa58c6459f9907
Behzad-Az <behy1368@gmail.com>
2021-09-16 18:11:39 -0700

    messages can now be sent

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,4 +3,7 @@ export { default as ChannelListContainer } from './ChannelListContainer.jsx';
 export { default as ChannelSearch } from './ChannelSearch.jsx';
 export { default as TeamChannelList } from './TeamChannelList.jsx';
 export { default as TeamChannelPreview } from './TeamChannelPreview.jsx';
+export { default as ChannelInner } from './ChannelInner.jsx';
+export { default as CreateChannel } from './CreateChannel.jsx';
+export { default as EditChannel } from './EditChannel.jsx';
 export { default as Auth } from './Auth.jsx';
